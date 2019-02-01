03dd0e7d815afa6cce6e0b8e8efa3df843564f1e
Kadi Kukk <kadikukk10@gmail.com>
2019-02-01 15:13:20 +0200

    Improve sign in page layout

--- src/sign/SignInPage.jsx
+++ src/sign/SignInPage.jsx
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
+import { Paper, TextField, RaisedButton } from 'material-ui';
+import { FormattedMessage } from 'react-intl';
 
 import { SignUpLink } from './SignUpPage.jsx';
 import { PasswordForgetLink } from '../password/PasswordForgetPage.jsx';
 import withFirebase from '../firebase/withFirebase';
 
-const SignInPage = () => (
-  <div>
-    <h1>SignIn</h1>
-    <SignInForm />
-    <PasswordForgetLink />
-    <SignUpLink />
-  </div>
-);
-
-const INITIAL_STATE = {
-  email: '',
-  password: '',
-  error: null,
+const errorMessages = {
+  'auth/user-not-found': 'errors.userNotFound',
+  'auth/wrong-password': 'errors.wrongPassword',
+  'auth/invalid-email': 'errors.invalidEmail'
 };
 
-class SignInFormBase extends Component {
+const SignInPage = () => <SignInForm />;
+
+const initialState = {
+  email: '',
+  password: '',
+  error: null
+};
+
+class SignInFormBase extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { ...INITIAL_STATE };
+    this.state = initialState;
   }
+
+  handleChange = (property, value) => {
+    this.setState({
+      [property]: value
+    });
+  }
+
+  emailChange = (email) => this.handleChange('email', email);
+
+  passwordChange = (password) => this.handleChange('password', password);
 
   onSubmit = event => {
     const { email, password } = this.state;
@@ -34,50 +45,81 @@ class SignInFormBase extends Component {
     this.props.firebase
       .doSignInWithEmailAndPassword(email, password)
       .then(() => {
-        this.setState({ ...INITIAL_STATE });
+        this.setState(initialState);
         this.props.history.push('/home');
       })
-      .catch(error => {
-        this.setState({ error });
-      });
+      .catch(error => this.setState({ error }));
 
     event.preventDefault();
   };
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
   render() {
-    const { email, password, error } = this.state;
-
-    const isInvalid = password === '' || email === '';
+    const isInvalid = this.state.password === '' || this.state.email === '';
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
-
-        {error && <p>{error.message}</p>}
-      </form>
+      <div style={{ margin: '70px auto' }}>
+        <Paper className="pagePaper">
+          <form className="formPadding" onSubmit={this.onSubmit}>
+            <div className="row">
+              <div className="col s12 m6">
+                <TextField
+                  floatingLabelText={<FormattedMessage id="signIn.email" />}
+                  value={this.state.email}
+                  onChange={(e, value) => this.emailChange(value)}
+                  fullWidth
+                />
+              </div>
+              <div className="col s12 m6">
+                <TextField
+                  type="password"
+                  floatingLabelText={<FormattedMessage id="signIn.password" />}
+                  value={this.state.password}
+                  onChange={(e, value) => this.passwordChange(value)}
+                  fullWidth
+                />
+              </div>
+            </div>
+            {this.state.error && (
+              <div className="row">
+                <div className="col s12" style={{ color: 'red' }}>
+                  {errorMessages[this.state.error.code]
+                    ? <FormattedMessage id={errorMessages[this.state.error.code]} />
+                    : this.state.error.message}
+                </div>
+              </div>
+            )}
+            <div className="row">
+              <div className="col s12" style={{ textAlign: 'right' }}>
+                <RaisedButton
+                  label={<FormattedMessage id="signIn.cancel" />}
+                  onClick={() => window.history.back()}
+                  style={{ marginRight: '20px' }}
+                />
+                <RaisedButton
+                  type="submit"
+                  primary
+                  label={<FormattedMessage id="signIn.submit" />}
+                  disabled={isInvalid}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s12">
+                <PasswordForgetLink />
+                <SignUpLink />
+              </div>
+            </div>
+          </form>
+        </Paper>
+      </div>
     );
   }
 }
+
+SignInFormBase.propTypes = {
+  firebase: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+};
 
 const SignInForm = compose(
   withRouter,
@@ -85,5 +127,3 @@ const SignInForm = compose(
 )(SignInFormBase);
 
 export default SignInPage;
-
-export { SignInForm };
