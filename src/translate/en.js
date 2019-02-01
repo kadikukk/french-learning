0143c1ec8c88bde591cb1d2374334dfc77681a85
Kadi Kukk <kadikukk10@gmail.com>
2019-02-01 15:13:46 +0200

    Write translations

--- src/translate/en.js
+++ src/translate/en.js
@@ -1,5 +1,19 @@
 const translations = {
-  'homePage.title': 'Home page'
+  'general.add': 'Add',
+  'appBar.menu': 'Menu',
+  'appBar.signIn': 'Sign in',
+  'appBar.signOut': 'Sign out',
+  'menu.chapters': 'Chapters',
+  'signIn.email': 'Email',
+  'signIn.password': 'Password',
+  'signIn.submit': 'Sign in',
+  'signIn.cancel': 'Cancel',
+  'errors.userNotFound': 'There is no user with this email',
+  'errors.wrongPassword': 'Entered password is invalid',
+  'errors.invalidEmail': 'Entered email is not correct',
+  'chapters.title': 'Chapters',
+  'chapter.name': 'Chapter\'s name',
+  'chapter.new': 'New chapter'
 };
 
 export default translations;
--- src/translate/et.js
+++ src/translate/et.js
@@ -1,5 +1,19 @@
 const translations = {
-  'homePage.title': 'Koduleht'
+  'general.add': 'Lisa',
+  'appBar.menu': 'Menüü',
+  'appBar.signIn': 'Logi sisse',
+  'appBar.signOut': 'Logi välja',
+  'menu.chapters': 'Peatükid',
+  'signIn.email': 'E-post',
+  'signIn.password': 'Parool',
+  'signIn.submit': 'Logi sisse',
+  'signIn.cancel': 'Katkesta',
+  'errors.userNotFound': 'Sellise e-posti aadressiga kasutajat ei leitud',
+  'errors.wrongPassword': 'Sisestatud parool on vale',
+  'errors.invalidEmail': 'Sisestatud e-posti aadress on vigane',
+  'chapters.title': 'Peatükid',
+  'chapter.name': 'Peatüki nimi',
+  'chapter.new': 'Uus peatükk'
 };
 
 export default translations;
