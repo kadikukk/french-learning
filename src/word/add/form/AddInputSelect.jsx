584828a21bcc7b74490fe0f11ef72e70fd1a789d
Kadi Kukk <kadikukk10@gmail.com>
2019-02-18 13:52:06 +0200

    Show message when from file is selected

--- src/word/add/form/AddInputSelect.jsx
+++ src/word/add/form/AddInputSelect.jsx
@@ -2,27 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { RadioButtonGroup, RadioButton } from 'material-ui';
+import { grey600 } from 'material-ui/styles/colors';
 
 const AddInputSelect = (props) => (
-  <div className="row">
-    <div className="col s12">
-      <RadioButtonGroup
-        name="wordAddInput"
-        valueSelected={props.selected}
-        style={{ display: 'flex' }}
-        onChange={(e, value) => props.selectedInputChange(value)}
-      >
-        <RadioButton
-          value="oneByOne"
-          label={<FormattedMessage id="words.add.input.oneByOne" />}
-        />
-        <RadioButton
-          value="fromFile"
-          label={<FormattedMessage id="words.add.input.file" />}
-        />
-      </RadioButtonGroup>
+  <React.Fragment>
+    <div className="row" style={{ marginTop: '30px' }}>
+      <div className="col s12 m12 l12">
+        <RadioButtonGroup
+          name="wordAddInput"
+          valueSelected={props.selected}
+          style={{ display: 'flex' }}
+          onChange={(e, value) => props.selectedInputChange(value)}
+        >
+          <RadioButton
+            value="oneByOne"
+            label={<FormattedMessage id="words.add.input.oneByOne" />}
+          />
+          <RadioButton
+            value="fromFile"
+            label={<FormattedMessage id="words.add.input.file" />}
+          />
+        </RadioButtonGroup>
+      </div>
     </div>
-  </div>
+    {props.selected === 'fromFile' ? (
+      <div className="row" style={{ marginTop: '30px' }}>
+        <div className="col s12 m12 l12" style={{ color: grey600 }}>
+          <FormattedMessage id="words.add.input.file.message" />
+        </div>
+      </div>
+    ) : ''}
+  </React.Fragment>
 );
 
 AddInputSelect.propTypes = {
