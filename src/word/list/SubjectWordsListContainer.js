d58ffcd6d17cbfcdb214a4f1d0532102c085ce0c
Kadi Kukk <kadikukk10@gmail.com>
2019-02-20 10:42:02 +0200

    List view for subject words

---
+++ src/word/list/SubjectWordsListContainer.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { filter } from 'ramda';
+import { CircularProgress } from 'material-ui';
+
+import withFirebase from '../../firebase/withFirebase';
+import SubjectWordsList from './SubjectWordsList';
+
+class SubjectWordsListContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: [],
+      fetching: false
+    };
+  }
+
+  componentDidMount() {
+    this.onListenForWords();
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.words().off();
+  }
+
+  onListenForWords = () => {
+    this.setState({ fetching: true });
+    this.props.firebase
+      .words()
+      .on('value', (snapshot) => {
+        const wordObject = snapshot.val();
+
+        if (wordObject) {
+          const wordList = Object.keys(wordObject).map(key => ({
+            ...wordObject[key],
+            uid: key
+          }));
+
+          this.setState({
+            words: wordList,
+            fetching: false
+          });
+        } else {
+          this.setState({
+            fetching: false
+          });
+        }
+      });
+  };
+
+  getWords = () => {
+    const pathnameParts = window.location.pathname.split('/');
+    const chapterIdLabel = pathnameParts[2];
+    const subjectIdLabel = pathnameParts[4];
+    const isSelectedChapterAndSubjectWord = (word) => (
+      word.chapterId.startsWith(chapterIdLabel) && word.subjectId.startsWith(subjectIdLabel)
+    );
+    return filter(isSelectedChapterAndSubjectWord, this.state.words);
+  };
+
+  renderLoader() {
+    return (
+      <div style={{ width: '100%', margin: 'auto', marginTop: '80px', textAlign: 'center' }}>
+        <CircularProgress size={80} thickness={7} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      this.state.fetching ? this.renderLoader() : (
+        <SubjectWordsList
+          words={this.getWords()}
+        />
+      )
+    );
+  }
+}
+
+SubjectWordsListContainer.propTypes = {
+  firebase: PropTypes.object.isRequired
+};
+
+export default withFirebase(SubjectWordsListContainer);
