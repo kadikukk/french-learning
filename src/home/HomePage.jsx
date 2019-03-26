7965098d5a843e64380c098c9bf31b9cc4846b40
Kadi Kukk <kadikukk10@gmail.com>
2019-03-26 14:35:27 +0200

    Move account not activated message style to const

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -5,10 +5,18 @@ import { FormattedMessage } from 'react-intl';
 import AuthUserContext from '../session/AuthUserContext';
 import { isActiveUser } from '../util/AuthUtil';
 
+const accountNotActivatedMessageStyle = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  display: 'flex',
+  height: '100px',
+  fontSize: '18px'
+};
+
 const AccountNotActivatedMessage = () => (
   <div style={{ margin: '70px auto' }}>
     <Paper className="pagePaper">
-      <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', height: '100px', fontSize: '18px' }}>
+      <div style={accountNotActivatedMessageStyle}>
         <FormattedMessage id="account.notActivated" />
       </div>
     </Paper>
