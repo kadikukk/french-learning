06e0dba512f1c160a77540525d689a6fd35419be
Kadi Kukk <kadikukk10@gmail.com>
2019-03-29 16:16:19 +0200

    Fix not applying styles to appbar action buttons

--- src/appbar/AppBar.jsx
+++ src/appbar/AppBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { compose } from 'recompose';
 import { AppBar as MaterialAppBar, FlatButton } from 'material-ui';
 import { FormattedMessage } from 'react-intl';
 import windowDimensions from 'react-window-dimensions';
@@ -11,42 +12,53 @@ import withFirebase from '../firebase/withFirebase';
 const TABLET_SCREEN_SIZE = 768;
 
 class AppBar extends React.Component {
-  signInOrOutButton = () => {
+  handleSignOut = () => {
+    this.props.firebase.signOut();
+    this.props.history.push('/');
+  };
+
+  signInOrOutButton = (authUser) => {
+    if (authUser) {
+      return (
+        <FlatButton
+          label={<FormattedMessage id="appBar.signOut" />}
+          onClick={this.handleSignOut}
+        />
+      );
+    }
     return (
-      <AuthUserContext.Consumer>
-        {authUser => authUser ? (
-          <Link to="/">
-            <FlatButton
-              label={<FormattedMessage id="appBar.signOut" />}
-              onClick={this.props.firebase.signOut}
-            />
-          </Link>)
-          : (
-            <Link to="/signin">
-              <FlatButton
-                label={<FormattedMessage id="appBar.signIn" />}
-              />
-            </Link>)}
-      </AuthUserContext.Consumer>
+      <FlatButton
+        label={<FormattedMessage id="appBar.signIn" />}
+        onClick={() => this.props.history.push('/signin')}
+      />
     );
   };
 
   render() {
     return (
-      <MaterialAppBar
-        title={<FormattedMessage id="appBar.menu" />}
-        onLeftIconButtonClick={this.props.toggleMenu}
-        iconElementRight={this.props.width > TABLET_SCREEN_SIZE ? this.signInOrOutButton() : null}
-      />
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <MaterialAppBar
+            title={<FormattedMessage id="appBar.menu" />}
+            onLeftIconButtonClick={this.props.toggleMenu}
+            iconElementRight={this.props.width > TABLET_SCREEN_SIZE ? this.signInOrOutButton(authUser) : null}
+          />
+        )}
+      </AuthUserContext.Consumer>
     );
   }
 }
 
 AppBar.propTypes = {
   width: PropTypes.number.isRequired,
+  history: PropTypes.object.isRequired,
   toggleMenu: PropTypes.func.isRequired,
   firebase: PropTypes.object.isRequired
 };
 
-export default windowDimensions()(withFirebase(AppBar));
+export default windowDimensions()(
+  compose(
+    withFirebase,
+    withRouter
+  )(AppBar));
 
