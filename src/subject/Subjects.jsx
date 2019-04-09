05494e71307282cfc0de905772b482500aacf1e6
Kadi Kukk <kadikukk10@gmail.com>
2019-04-09 11:43:19 +0300

    Visually improve subjects page

--- src/subject/Subjects.jsx
+++ src/subject/Subjects.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Paper, GridList, GridTile } from 'material-ui';
+import { Paper } from 'material-ui';
+
+import { idLabel } from '../util/IdLabel';
 
 import './Subjects.css';
-import { grey600 } from 'material-ui/styles/colors';
-import { idLabel } from '../util/IdLabel';
 
 const Subjects = (props) => (
   <div style={{ margin: '70px auto', maxWidth: '750px' }}>
-    <GridList cols={2} cellHeight="auto" padding={9}>
+    <div className="row">
       {props.subjects.map((subject) => (
-        <Link
-          key={subject.uid}
-          to={`/chapters/${idLabel(subject.chapterId)}/subjects/${idLabel(subject.uid)}/words`}
-        >
-          <GridTile style={{ boxShadow: '2px 4px #E0E0E0', border: '1px solid #E0E0E0' }}>
+        <div key={subject.uid} className="col s6 m6 l6">
+          <Link
+            to={`/chapters/${idLabel(subject.chapterId)}/subjects/${idLabel(subject.uid)}/words`}
+          >
             <Paper className="subjectPaper">
-              <div style={{ fontSize: '20px', color: grey600 }}>
+              <div style={{ fontSize: '20px', color: '#5e35b1' }}>
                 <b>{subject.name.toUpperCase()}</b>
               </div>
             </Paper>
-          </GridTile>
-        </Link>
+          </Link>
+        </div>
       ))}
-    </GridList>
+    </div>
   </div>
 );
 
