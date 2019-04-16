7869c37d3c8ad29c12b17537f62a3e30a6fe534d
Kadi Kukk <kadikukk10@gmail.com>
2019-04-16 10:02:02 +0300

    Add chapter row delete trigger function

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,11 @@ const functions = require('firebase-functions');
 
 // This will run when a row is changed that matches this pattern
 exports.onDeletedChapterRow = functions.database.ref('/chapters/{chapterId}')
-  .onDelete((event) => {
+  .onUpdate((event) => {
+    // Exit if this item exists... if so it was not deleted!
+    if (event.data.exists()) {
+      return;
+    }
     // Remove all subjects from that chapter
     event.data.adminRef.getRoot().ref('/subjects')
       .orderByChild('chapterId')
