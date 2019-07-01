c7f596713c4a868b8bf0bef68633c6e10a772a9c
Kadi Kukk <kadikukk10@gmail.com>
2019-07-01 16:08:31 +0300

    Change project name

--- src/app/PrivacyPolicy.jsx
+++ src/app/PrivacyPolicy.jsx
@@ -8,11 +8,11 @@ const PrivacyPolicy = () => (
     <p>Effective date: June 17, 2019</p>
 
 
-    <p>Your French Assistant ("us", "we", or "our") operates the https://french-learning-application.firebaseapp.com/ website (the "Service").</p>
+    <p>Your French Assistant ("us", "we", or "our") operates the https://yourfrassistant.firebaseapp.com/ website (the "Service").</p>
 
     <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. Our Privacy Policy  for Your French Assistant is created with the help of the <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator.php">Free Privacy Policy Generator</a>.</p>
 
-    <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://french-learning-application.firebaseapp.com/</p>
+    <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://yourfrassistant.firebaseapp.com/</p>
 
 
     <h2>Information Collection And Use</h2>
